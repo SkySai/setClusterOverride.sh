dddb84990b8ac3290612632319159679083ed53f
Vincent He <vincent.dexter.he@gmail.com>
2019-06-05 13:18:40 -0500

    Add files via upload

---
+++ setClusterOverride.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+auth="admin:admin"
+
+if [ "$#" -eq 1 ]; then
+    ip=$1
+elif [ "$#" -eq 2 ]; then
+    auth=$1
+    ip=$2
+else
+    echo "USAGE: setSipDetails [auth] ip"
+    echo "auth: specify username and password when non default (admin:admin) include the colon to separate the username from the password"
+    echo "ip: specify endpoint IP address"
+    exit 1
+fi
+
+# 0 = SIP is not enabled
+# 1 = SIP is enabled
+bOverrideClusterDiscovery="0"
+cssTargetNode='169.46.49.132'
+bIsStatsCollectionEnabled='1'
+#eContextType="0"
+
+echo "AUTH is ${auth}"
+b64auth=$(echo -n ${auth} | base64)
+echo "IP is ${ip}"
+
+session=$( curl -H "Authorization: LSBasic ${b64auth}" -H "Content-Type: application/json" http://$ip/rest/new | awk -F\" '/session/ { print $4 }' )
+echo "SESSION is ${session}"
+
+curl -H "Authorization: LSBasic ${b64auth}" -H "Content-Type: application/json" --data "{\"call\":\"Comm_setCSSIntegratedModeSupportDetails\",\"params\":{\"pCSSIntegratedModeSupportDetails\":{\"bOverrideClusterDiscovery\":${bOverrideClusterDiscovery},\"bIsStatsCollectionEnabled\":\"${bIsStatsCollectionEnabled}\",\"cssTargetNode\":\"${cssTargetNode}\"}}}" http://${ip}/rest/request/${session} 
+
+curl -H "Authorization: LSBasic ${b64auth}" -H "Content-Type: application/json" --data "{\"params\":{},\"call\":\"SysAdmin_getTime\"}" http://${ip}/rest/request/${session} 
+
